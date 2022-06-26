19b96878c1da889d3ad928ad1698d7c209567602
Hanriel <hanriel59@gmail.com>
2022-06-26 05:36:46 +0500

    fix: some dirt in code

--- index.php
+++ index.php
@@ -13,12 +13,5 @@ spl_autoload_register(function ($class) {
 
 session_start();
 
-define('OAUTH2_CLIENT_ID', '855378227957989407');
-define('OAUTH2_CLIENT_SECRET', 'rsT5OU_VOkdecFaTvxS7fU6qmsoNJwtz');
-
-$authorizeURL = 'https://discord.com/api/oauth2/authorize';
-$tokenURL = 'https://discord.com/api/oauth2/token';
-$apiURLBase = 'https://discord.com/api/users/@me';
-
 $router = new Router;
 $router->run();
